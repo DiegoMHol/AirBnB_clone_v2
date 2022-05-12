783a96f703b6fdccb2e1bfcd363385709d7b010d
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-12 12:18:05 -0700

    [HBNB ROUTE]

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,11 +6,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """ String Hello HBNB """
-    return 'Hello HBNB!'
-
 @app.route('/hbnb', strict_slashes=False)
 def hello_world_hbnb():
     """ String HBNB return """
