ccf9def1f581e0864935ea761e7c46e3179c343e
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-12 12:30:05 -0700

    [C + TEXT VAR]

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ return string HELLO HBNB """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world_hbnb():
+    """ String HBNB return """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_world_text(text):
+    """ return string c + text var """
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
