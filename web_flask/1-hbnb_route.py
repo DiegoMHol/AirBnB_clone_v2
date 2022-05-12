325b385afd2096b725af397907381717c1c9a496
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-12 12:21:13 -0700

    [ROUTE HBNB AND '/']

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,6 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ return string HELLO HBNB """
+    return 'Hello HBNB!'
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_world_hbnb():
     """ String HBNB return """
