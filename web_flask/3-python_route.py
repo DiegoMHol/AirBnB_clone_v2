ac5cd38f71786614400349ba369c74d12f4c26c4
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-12 12:44:15 -0700

    [PYTOHN + VAR]

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ return string HELLO HBNB """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world_hbnb():
+    """ String HBNB return """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_world_text(text):
+    """ return string c + text var """
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_world_python(text='is cool'):
+    """ return string c + text var """
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
